78bd39db55a8af5cf0cdcf9e92885fb479a94dc6
stebal <stephane.baldino@gmail.com>
2018-07-24 22:10:22 +0200

    Creation d'un ville : BasedeDonnées

--- app/src/main/java/com/stebal/stebalmeteo/Database.kt
+++ app/src/main/java/com/stebal/stebalmeteo/Database.kt
@@ -1,8 +1,11 @@
 package com.stebal.stebalmeteo
 
+import android.content.ContentValues
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
+import android.util.Log
+import com.stebal.stebalmeteo.city.City
 
 private const val DATABASE_NAME = "weather.db"
 private const val DATABASE_VERSION = 1
@@ -21,11 +24,26 @@ private const val CITY_TABLE_CREATE = """
 class Database(context: Context)
     : SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
 
+    val TAG = Database::class.java.simpleName
+
     override fun onCreate(db: SQLiteDatabase?) {
            db?.execSQL(CITY_TABLE_CREATE)
     }
 
     override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
 
+    }
+
+    fun createCity(city : City) : Boolean {
+
+        val values = ContentValues()
+        values.put(CITY_KEY_NAME, city.name)
+
+        Log.d(TAG, "Creating city : $values")
+
+        val id = writableDatabase.insert(CITY_TABLE_CREATE, null, values)
+        city.id = id
+
+        return id > 0
     }
 }
